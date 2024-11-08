a58cfa1e5c0b9eba4042634bad0243bbd0115988
Jacques-Etienne Beaudet <jebeaudet@coveo.com>
2024-11-08 15:49:31 -0500

    repro projet for gh-12505

---
+++ src/main/java/com/example/jetty/JettyGh12505Application.java
@@ -0,0 +1,12 @@
+package com.example.jetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JettyGh12505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JettyGh12505Application.class, args);
+	}
+}
